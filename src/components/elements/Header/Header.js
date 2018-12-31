0690a87cf3c75843667881722a4e5328a7cf4539
Danielle Moss <dani.code.camp@gmail.com>
2018-12-30 22:55:43 -0500

    Added routing with React Router for the movie thumbnails

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className='rmdb-header'>
       <div className='rmdb-header-content'>
-        <img
-          className='rmdn-logo'
-          src='./images/reactMovie_logo.png'
-          alt='React Movie Database Logo'
-        />
+        <Link to='/'>
+          <img
+            className='rmdn-logo'
+            src='./images/reactMovie_logo.png'
+            alt='React Movie Database Logo'
+          />
+        </Link>
+
         <img
           className='rmdb-tmdb-logo'
           src='./images/tmdb_logo.png'
